b730b5490477a5b6f9a1e30cec9848e3a4d60dad
agilul <agilulfo89@gmail.com>
2016-03-03 14:18:25 +0100

    Added files via upload

---
+++ main.lua
@@ -0,0 +1,130 @@
+vector = require "vector"
+matrix = require "matrix"
+
+function love.load()
+	imgWidth, imgHeight = 600, 600
+	love.window.setMode(imgWidth, imgHeight)
+	screenCenter = vector(imgWidth/2, imgHeight/2)
+	love.mouse.setPosition(screenCenter.x, screenCenter.y)
+	love.mouse.setVisible(false)
+	
+	-- loads the vertex list of a Newell teapot
+	local index = 1
+	teapot = {}
+	for line in love.filesystem.lines("teapot") do
+		local _, _, x, y, z = string.find(line, "(%-?%d+%.%d+),(%-?%d+%.%d+),(%-?%d+%.%d+)")
+		teapot[index] = vector(x, y, z)
+		index = index + 1
+	end
+	
+	forwardVec = vector(0, 0, -1)
+	upVec = vector(0, 1, 0)
+	leftVec = upVec:cross(forwardVec)
+	cameraMatrix = matrix.createIdentityMatrix()
+	cameraMatrix[4][2] = 5
+	cameraMatrix[4][3] = 10
+	worldToCamera = matrix.inverse(cameraMatrix)
+	
+	-- creates a perspective projection matrix with FoV, near and far planes
+	projMatrix = matrix.createProjectionMatrix(90, 0.1, 100)
+	
+	instructions = true
+	instructionsTimeout = 10
+end
+
+function love.update(dt)
+	if love.keyboard.isDown("escape") then
+		love.event.quit()
+	end
+	
+	local rotationVec, rotationAngle = vector(1, 0, 0), 0
+	local dMousePos = vector(love.mouse.getPosition()) - screenCenter
+	love.mouse.setPosition(imgWidth/2, imgHeight/2)
+	
+	-- yaw
+	if math.abs(dMousePos.x) > 0 then
+		rotationVec = upVec
+		rotationAngle = -dMousePos.x * dt / 30
+	end
+	cameraMatrix = matrix.multiply(matrix.createRotationMatrix(rotationVec, rotationAngle), cameraMatrix)
+	
+	-- pitch
+	if math.abs(dMousePos.y) > 0 then
+		rotationVec = leftVec
+		rotationAngle = -dMousePos.y * dt / 30
+	end
+	cameraMatrix = matrix.multiply(matrix.createRotationMatrix(rotationVec, rotationAngle), cameraMatrix)
+	
+	-- roll
+	if love.keyboard.isDown("q") then
+		rotationVec = forwardVec
+		rotationAngle = 1 * dt
+	elseif love.keyboard.isDown("e") then
+		rotationVec = forwardVec
+		rotationAngle = -1 * dt
+	end
+	cameraMatrix = matrix.multiply(matrix.createRotationMatrix(rotationVec, rotationAngle), cameraMatrix)
+	
+	-- panning controls
+	local translateVec = vector(0, 0, 0)
+	
+	if love.keyboard.isDown("w") or love.keyboard.isDown("up") then
+		translateVec = translateVec - upVec * dt
+	elseif love.keyboard.isDown("s") or love.keyboard.isDown("down") then
+		translateVec = translateVec + upVec * dt
+	end
+	
+	if love.keyboard.isDown("a") or love.keyboard.isDown("left") then
+		translateVec = translateVec + leftVec * dt
+	elseif love.keyboard.isDown("d") or love.keyboard.isDown("right") then
+		translateVec = translateVec - leftVec * dt
+	end
+	
+	if love.keyboard.isDown("r") then
+		translateVec = translateVec - forwardVec * dt
+	elseif love.keyboard.isDown("f") then
+		translateVec = translateVec + forwardVec * dt
+	end
+	
+	translateVec:normalizeInplace()
+	cameraMatrix = matrix.multiply(matrix.createTranslationMatrix(translateVec), cameraMatrix)
+
+	-- move by moving the rest
+	-- http://futurama.wikia.com/wiki/Dark_matter_engine
+	worldToCamera = matrix.inverse(cameraMatrix)
+
+	-- transforms the world from 3D space to 2D screen
+	points = {}
+	index = 0 
+	for i, v in ipairs(teapot) do
+		local cameraVert = v:multVecMatrix(worldToCamera)
+		if cameraVert * forwardVec > 0 then
+			local projected = cameraVert:multVecMatrix(projMatrix)
+			if projected.x >= -1 and projected.x <= 1 and projected.y >= -1 and projected.y <= 1 then
+				local screen_x = math.min(imgWidth - 1, math.floor((projected.x + 1) * 0.5 * imgWidth))
+				local screen_y = math.min(imgHeight - 1, math.floor((projected.y + 1) * 0.5 * imgHeight))
+				points[index] = {x = screen_x, y = screen_y}
+				index = index + 1
+			end
+		end
+	end
+	
+	if instructions then
+		if instructionsTimeout < 0 then
+			instructions = false
+		end
+		instructionsTimeout = instructionsTimeout - dt
+	end
+end
+
+function love.draw()
+	love.graphics.setPointSize(2)
+	for i, v in ipairs(points) do
+		love.graphics.point(v.x, v.y)
+	end
+	
+	if instructions then
+		love.graphics.setColor(255, 255, 255)
+		love.graphics.printf("WASD to move up/left/down/right\nF/R to move forward/backward\nMouse to yaw/pitch\nQ/E to roll\nESC to exit", 0, 0, imgWidth, "left", 0, 2)
+	end
+end
---
+++ vector.lua
@@ -0,0 +1,113 @@
+local sqrt = math.sqrt
+
+local vector = {}
+vector.__index = vector
+
+local function new(x, y, z)
+	return setmetatable({x = x or 0, y = y or 0, z = z or 0}, vector)
+end
+
+local zero = new(0, 0, 0)
+
+local function isvector(v)
+	return type(v) == 'table' and type(v.x) == 'number' and type(v.y) == 'number' and type(v.z) == 'number'
+end
+	
+function vector:clone()
+	return new(self.x, self.y, self.z)
+end
+
+function vector:unpack()
+	return self.x, self.y, self.z
+end
+
+function vector:__tostring()
+	return "(" .. tonumber(self.x) .. "," .. tonumber(self.y) .. "," .. tonumber(self.z) .. ")"
+end
+
+-- unary minus operator
+function vector.__unm(a)
+	return new(-a.x, -a.y, -a.z)
+end
+
+function vector.__add(a, b)
+	return new(a.x + b.x, a.y + b.y, a.z + b.z)
+end
+
+function vector.__sub(a, b)
+	return new(a.x - b.x, a.y - b.y, a.z - b.z)
+end
+
+-- dot product
+function vector.__mul(a, b)
+	if type(a) == "number" then
+		return new(a * b.x, a * b.y, a * b.z)
+	elseif type(b) == "number" then
+		return new(b * a.x, b * a.y, b * a.z)
+	else
+		return a.x * b.x + a.y * b.y + a.z * b.z
+	end
+end
+
+function vector.__div(a, b)
+	return new(a.x / b, a.y / b, a.z / b)
+end
+
+function vector.__eq(a, b)
+	return a.x == b.x and a.y == b.y and a.z == b.z
+end
+
+-- squared length
+function vector:len2()
+	return self.x * self.x + self.y * self.y + self.z * self.z
+end
+
+function vector:len()
+	return sqrt(self.x * self.x + self.y * self.y + self.z * self.z)
+end
+
+-- squared distance
+function vector.dist2(a, b)
+	local dx = a.x - b.x
+	local dy = a.y - b.y
+	local dz = a.z - b.z
+	return dx * dx + dy * dy + dz * dz
+end
+
+function vector.dist(a, b)
+	local dx = a.x - b.x
+	local dy = a.y - b.y
+	local dz = a.z - b.z
+	return sqrt(dx * dx + dy * dy + dz * dz)
+end
+
+function vector:normalizeInplace()
+	local l = self:len()
+	if l > 0 then
+		self.x, self.y, self.z = self.x / l, self.y / l, self.z / l
+	end
+	return self
+end
+
+function vector:normalized()
+	return self:clone():normalizeInplace()
+end
+
+-- cross product
+function vector:cross(v)
+	return new(self.y * v.z - self.z * v.y, self.z * v.x - self.x * v.z, self.x * v.y - self.y * v.x)
+end
+
+-- using homogeneous representation
+function vector.multVecMatrix(v, m)
+	local a, b, c, w
+	a = v.x * m[1][1] + v.y * m[2][1] + v.z * m[3][1] + m[4][1]
+	b = v.x * m[1][2] + v.y * m[2][2] + v.z * m[3][2] + m[4][2]
+	c = v.x * m[1][3] + v.y * m[2][3] + v.z * m[3][3] + m[4][3]
+	w = v.x * m[1][4] + v.y * m[2][4] + v.z * m[3][4] + m[4][4]
+	return new(a, b, c) / w
+end
+
+return setmetatable({new = new, isvector = isvector, zero = zero},
+	{__call = function(_, ...) return new(...) end})
+	
